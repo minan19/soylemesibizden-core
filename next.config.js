8fb686e192b033acc32dccd73ddecbe7ee0b571f
minan19 <mustafaamirinan@gmail.com>
2026-04-13 00:58:04 +0300

    Sovereign Override: Ignoring all build errors for global launch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  swcMinify: true
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
+
 module.exports = nextConfig;
